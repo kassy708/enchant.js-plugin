8d1774c9a0c40465754fb47533cc4d2ac5274aa4
kassy708 <kassysmilingtogether@gmail.com>
2012-02-11 02:00:58 +0900

    コメント文修正

--- PhySprite/examples/sample1/PhySprite.enchant.js
+++ PhySprite/examples/sample1/PhySprite.enchant.js
@@ -1,38 +1,37 @@
 /**
-*PhySprite.enchant.js v1.30
+*PhySprite.enchant.js
+* @version 1.30
+* @require enchant.js v0.4.3 or later
+* @require Box2dWeb.js
+* @author kassy708 http://twitter.com/kassy708
 *
-*The MIT License
+* @description
+* 物理演算用のSprite
+* このプラグインではBox2dWeb.jsを用いています。
+* 最新のBox2dWeb.jsは下記アドレスからダウンロードしてください。
+* http://www.gphysics.com/
 *
-*Copyright (c) 2012/01/26 kassy708
+* @license
+* The MIT License
+* Copyright (c) 2012/01/26 kassy708
 *
-*Permission is hereby granted, free of charge, to any person obtaining a copy
-*of this software and associated documentation files (the "Software"), to deal
-*in the Software without restriction, including without limitation the rights
-*to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-*copies of the Software, and to permit persons to whom the Software is
-*furnished to do so, subject to the following conditions:
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
 *
-*The above copyright notice and this permission notice shall be included in
-*all copies or substantial portions of the Software.
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
 *
-*THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-*IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-*FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-*AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-*LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-*OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-*THE SOFTWARE.
-*
-*
-*
-*
-*物理演算用のSprite
-*
-*@author kassy708 http://twitter.com/kassy708
-*
-*このプラグインではBox2dWeb.jsを用いています。
-*最新のBox2dWeb.jsは下記アドレスからダウンロードしてください。
-*http://www.gphysics.com
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
 */
 
 
